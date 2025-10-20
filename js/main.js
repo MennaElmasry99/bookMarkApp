b1cc89cb57ef32505e4b35c4b41d8338d866e534
MennaElmasry99 <menna.khaled.elmasry@gmail.com>
2025-10-20 03:56:02 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// general variables
+var bookmarkNameInput = document.getElementById("bookmarkName");
+var webSiteUrlInput = document.getElementById("webSiteUrl");
+var tableBody = document.getElementById("tableBody");
+var bookmarkList = [];
+
+// load from localStorage if exists
+if (localStorage.getItem("bookmarkList") != null) {
+  bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
+  displayBookmark();
+}
+
+// add url function
+function addUrl() {
+  var urlObject = {
+    name: bookmarkNameInput.value.trim(),
+    url: webSiteUrlInput.value.trim()
+  };
+
+  if (urlObject.name && urlObject.url) {
+    bookmarkList.push(urlObject);
+    localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
+    displayBookmark();
+    clearInputs();
+  } else {
+    alert("Please fill out both fields!");
+  }
+}
+
+// clear inputs
+function clearInputs() {
+  bookmarkNameInput.value = "";
+  webSiteUrlInput.value = "";
+}
+
+// display bookmark function
+function displayBookmark() {
+  var trs = "";
+  for (var i = 0; i < bookmarkList.length; i++) {
+    trs += `
+      <tr>
+        <td>${i + 1}</td>
+        <td>${bookmarkList[i].name}</td>
+        <td>
+          <a href="${bookmarkList[i].url}" target="_blank" class="btn btn-success text-white">
+            <i class="fa-solid fa-eye pe-2"></i> Visit
+          </a>
+        </td>
+        <td>
+          <button type="button" class="btn btn-danger text-white" onclick="deleteUrl(${i})">
+            <i class="fa-solid fa-trash pe-2"></i> Delete
+          </button>
+        </td>
+      </tr>`;
+  }
+  tableBody.innerHTML = trs;
+}
+
+// delete url
+function deleteUrl(index) {
+  bookmarkList.splice(index, 1);
+  localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
+  displayBookmark();
+}
